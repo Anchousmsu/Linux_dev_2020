6386fcb2af6fe8eec3c0bf21cbf63c1c6d71cc13
Anchousmsu <NUT1997020501@yandex.ru>
2021-01-03 19:04:35 +0300

    Add file scrolling

--- 03_TerminalProject/Show.c
+++ 03_TerminalProject/Show.c
@@ -1,17 +1,79 @@
 #include <ncurses.h>
 #include <stdlib.h>
 #include <string.h>
+#include <locale.h>
 
 #define DX 3
+#define NUM_LEN 4
 #define KILO 1024
 
-int main(int argc, char *argv[]) {
+
+int shift_num(int num)
+{
+    int len = 0;
+    while (num > 0)
+    {
+        num /= 10;
+        len++;
+    }
+    return NUM_LEN - len;
+}
+
+
+char *shift_line(int num)
+{
+    int len = shift_num(num);
+    char *shift = malloc((len + 1) * sizeof(char));
+    int i = 0;
+    shift[i++] = ' ';
+    while (len)
+    {
+        shift[i++] = ' ';
+        len--;
+    }
+    return shift;
+}
+
+
+char *strtok_new(char * string, char const * delimiter)
+{
+    static char *source = NULL;
+    char *p, *res = 0;
+    if (string)
+        source = string;
+    if(!source)
+        return NULL;
+    if((p = strpbrk (source, delimiter)) != NULL)
+    {
+      *p  = 0;
+      res = source;
+      source = ++p;
+    }
+    return res;
+}
+
+
+void print_cur_lines(WINDOW *win, char **lines, int first, int last, int left, int right)
+{
+    for (int i = first; i <= last; i++)
+    {
+        int n_line = i + 1;
+        wprintw(win, "%s%d: %s\n", shift_line(n_line), n_line, lines[i]);
+    }
+    box(win, 0, 0);
+    wrefresh(win);
+}
+
+
+int main(int argc, char *argv[])
+{
     FILE *infile;
     char *buffer, **lines;
     long size_memory;
 
     WINDOW *win;
     int c = 0;
+    int first, last, left, right;
 
     if (argc < 2)
     {
@@ -19,6 +81,7 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
+    // чтение файла
     infile = fopen(argv[1], "r");
     if(!infile)
         return 1;
@@ -27,13 +90,15 @@ int main(int argc, char *argv[]) {
 
     fseek(infile, 0L, SEEK_SET);
 
-    buffer = malloc(size_memory*sizeof(char));
+    buffer = malloc(size_memory * sizeof(char));
     if(!buffer)
         return 1;
 
     fread(buffer, sizeof(char), size_memory, infile);
     fclose(infile);
 
+    // подготовка ncurses
+    setlocale(LC_ALL, "");
     initscr();
     noecho();
     cbreak();
@@ -45,26 +110,55 @@ int main(int argc, char *argv[]) {
     keypad(win, TRUE);
     scrollok (win, TRUE);
 
-    int cur_start_file = 0;
-    char *split = strtok(buffer,"\n");
-    for (int i = 0; i < num_lines && split; i++)
+    // заполнение массива строк и вывод первых в окно
+    first = left = 0;
+    last = num_lines - 3;
+    right = num_cols - 3;
+    char *split = strtok_new(buffer,"\n");
+    int size_lines = 2, n_lines = 0;
+    wprintw(win, "\n");
+    lines = malloc(size_lines * sizeof(char*));
+    for (; split; n_lines++)
     {
-        wprintw(win, " %s\n", split);
-        split = strtok(NULL,"\n");
+        if (n_lines >= size_lines)
+        {
+            size_lines *= 2;
+            lines = realloc(lines, size_lines * sizeof(char*));
+        }
+        lines[n_lines] = split;
+        split = strtok_new(NULL,"\n");
+        if (n_lines < num_lines - 2)
+        {
+            int n_line = n_lines + 1;
+            wprintw(win, "%s%d: %s\n", shift_line(n_line), n_line, lines[n_lines]);
+        }
     }
+    n_lines--;
     box(win, 0, 0);
     wrefresh(win);
+
+    // обработка нажатия на клавиши
     while ((c = wgetch(win)) != 27)
     {
         switch(c)
         {
             case KEY_DOWN:
-                if (split)
+                if (last < n_lines)
                 {
-                    wprintw(win, "%s\n", split);
+                    last++;
+                    first++;
+                    int n_line = last + 1;
+                    wprintw(win, "%s%d: %s\n", shift_line(n_line), n_line, lines[last]);
                     box(win, 0, 0);
                     wrefresh(win);
-                    split = strtok(NULL,"\n");
+                }
+                break;
+            case KEY_UP:
+                if (first > 0)
+                {
+                    first--;
+                    last--;
+                    print_cur_lines(win, lines, first, last, left, right);
                 }
                 break;
             default:
@@ -75,6 +169,7 @@ int main(int argc, char *argv[]) {
     endwin();
 
     free(buffer);
+    free(lines);
 
     return 0;
 }
