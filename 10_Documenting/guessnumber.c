e128461677d450532bbb9018c306faf4815cbc7a
Anchousmsu <NUT1997020501@yandex.ru>
2021-01-09 19:41:32 +0300

    Add doxygen and --help string

--- 10_Documenting/guessnumber.c
+++ 10_Documenting/guessnumber.c
@@ -1,19 +1,47 @@
+/*! \file */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <libintl.h>
 #include <locale.h>
 
+/*!
+\def _(STRING)
+\brief macros for translation texts
+*/
 #define _(STRING) gettext(STRING)
 
+#define HELP _("\
+Usage: ./guessnumber [OPTION]\n\
+	\n\
+	-r			enable roman numbers\n\
+	\n\
+EXECUTABLE should accept \'--help\' options and produce output on\n\
+stdout:\n\
+	\n\
+	-h, --help		help option string\n\
+	")
+
+/*!
+\struct RomanTable
+\brief struct for for matching numbers
+*/
 typedef struct
 {
-	int number;
-	char roman[10];
+	int number; /// arabic number
+	char roman[10]; /// roman number
 }RomanTable;
 
+/// Table of matching numbers from 1 to 100
 RomanTable roman_table[100];
 
+
+/*!
+\brief function for translation from roman number to arabic number
+\param[in] roman roman numbers
+\return arabic number
+*/
 int fromRoman(char *roman)
 {
 	for (int i = 0; i < 100; i++)
@@ -24,6 +52,11 @@ int fromRoman(char *roman)
 	return -1;
 }
 
+/*!
+\brief function for translation from arabic number to roman number
+\param[in] number arabic numbers
+\return roman number
+*/
 char* toRoman(int number)
 {
 	if (number > 0 && number <= 100)
@@ -32,6 +65,10 @@ char* toRoman(int number)
 		return "";
 }
 
+/*!
+\brief function for reading roman table from file
+\param[in] name_file file name
+*/
 void readRomanTableFile(const char *name_file)
 {
 	FILE *fp;
@@ -63,10 +100,14 @@ int main(int argc, char *argv[])
 
 	int flag_roman = 0;
 
-	if (argc == 2 && !strncmp(argv[1], "-r", 2))
+	if (argc >= 2 && !strncmp(argv[1], "-r", 2))
 	{
 		flag_roman = 1;
 		readRomanTableFile("romantable.txt");
+	}
+	else if (argc >= 2 && (!strncmp(argv[1], "--help", 6) || !strncmp(argv[1], "-h", 2)))
+	{
+		return printf("%s\n", HELP);
 	}
 
 	if (flag_roman)
